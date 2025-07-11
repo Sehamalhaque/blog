1655a49f6ace9b9cd7c1371940f4152d2dd57d0b
Seham@GITS <seham.al.haque@g.bracu.ac.bd>
2025-07-11 21:22:47 +0600

    pip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'api',
     'corsheaders',
     'rest_framework',
