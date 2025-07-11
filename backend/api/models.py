7858273f1b90e9a3486a51b2c7f11468bf1a2177
Seham@GITS <seham.al.haque@g.bracu.ac.bd>
2025-07-11 23:15:38 +0600

    models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.utils.html import mark_safe
+from django.utils.text import slugify
+
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
