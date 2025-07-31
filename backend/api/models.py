b74d466f090894ce4f34376a87879b6cb1f2fc93
Seham@GITS <seham.al.haque@g.bracu.ac.bd>
2025-07-31 23:06:44 +0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,65 +1,45 @@
+# api/models.py
 from django.db import models
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
-from django.utils.html import mark_safe
-from django.utils.text import slugify
 
-from shortuuid.django_fields import ShortUUIDField
-import shortuuid
+# Custom User Manager
+class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-# Create your models here.
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        extra_fields.setdefault('username', email.split('@')[0])
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('username', email.split('@')[0])
+        return self.create_user(email, password, **extra_fields)
+
+# Custom User Model
 class User(AbstractUser):
-    username = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True, blank=True, null=True)
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255, blank=True, null=True)
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
-    
+
+    objects = UserManager()
+
     def __str__(self):
         return self.email
-    
-    def save(self, *args, **kwargs):
-        email_username, mobile = self.email.split("@")
-        if self.full_name == "" or self.full_name == None:
-            self.full_name = email_username
-        if self.username == "" or self.username == None:
-            self.username = email_username  
-    
-        super(User, self).save(*args, **kwargs)
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
-    full_name = models.CharField(max_length=100, null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    about = models.TextField(null=True, blank=True)
-    author = models.BooleanField(default=False)
-    country = models.CharField(max_length=100, null=True, blank=True)
-    facebook = models.CharField(max_length=100, null=True, blank=True)
-    twitter = models.CharField(max_length=100, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        if self.full_name:
-            return str(self.full_name)
-        else:
-            return str(self.user.full_name)
-    
 
     def save(self, *args, **kwargs):
-        if self.full_name == "" or self.full_name == None:
-            self.full_name = self.user.full_name
-        super(Profile, self).save(*args, **kwargs)
-    
-    
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
+        if not self.username:
+            self.username = self.email.split('@')[0]
+        if not self.full_name:
+            self.full_name = self.username
+        super().save(*args, **kwargs)
